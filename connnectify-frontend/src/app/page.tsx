5e29f4025fa29146107513520eef1fcbfb3834c8
Adeel <adeeltahir6a@gmail.com>
2025-07-02 12:36:44 +0500

    --created and backend structure too--

--- connnectify-frontend/src/app/page.tsx
+++ connnectify-frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import TierSection from "@/components/tier-section"
 import OurServices from "@/components/our-services"
 export default function Home(){
   return (
-    <section className="flex flex-col items-center justify-center bg-slate ">
+    <section className="flex flex-col items-center justify-center bg-[#673778]">
       <HeroSection />
       <OurServices />
       <TierSection />
